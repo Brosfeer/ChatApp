a01af0dd281e4e107b8ffc10c55994fdb505740d
Sharaf <alshykhshrfaldyn100@gmail.com>
2023-04-10 22:52:59 +0300

    first_commit

---
+++ app/src/main/java/com/example/chatapp/models/ChatMessage.java
@@ -0,0 +1,10 @@
+package com.example.chatapp.models;
+
+import java.util.Date;
+
+public class ChatMessage {
+    public String SendId, receiverId, message, dateTime;
+    public String senderId;
+    public Date dateObject;
+    public String conversionId,conversionName,conversionImage;
+}
